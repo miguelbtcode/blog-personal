b1b6b50e45debf4772069de713c01390afb4f1ac
Miguel Barreto <102126994+miguelbtcode@users.noreply.github.com>
2025-07-15 17:38:54 -0500

    Delete .next directory

--- .next/static/chunks/src_app_layout_tsx_68b267f5._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__8c5b3ecc._.css",
-  "static/chunks/node_modules_34b25729._.js",
-  "static/chunks/src_1edfa8a8._.js"
-],
-    source: "dynamic"
-});
